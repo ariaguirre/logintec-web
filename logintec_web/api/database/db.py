5493344dab98221378939809fd95536b81a82b05
ariaguirre <ariaguirre64@gmail.com>
2023-08-11 15:33:43 -0300

    commit

---
+++ logintec_web/api/database/db.py
@@ -0,0 +1,17 @@
+import mysql.connector 
+
+
+def get_connection():
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                         database='aconcagua',
+                                         user='root',
+                                         password='Cubiscan2023')
+        print('Succesfully connected to Database.')
+    except mysql.connector.Error as error:
+        print("Failed connect"(error))
+
+    finally:
+        if connection.is_connected():
+            connection.close()
+            print("MySQL connection is closed")
